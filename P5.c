8994b5b272102ea53c4c9f76e98db92fb7c73bbc
Bruno Damasceno Reis <38166890+brunodreis@users.noreply.github.com>
2018-07-05 19:40:50 -0300

    Avaliação Continuada P5

--- P5.c
+++ P5.c
@@ -1,54 +1,76 @@
 // # MTP2018-1
-// P5
 // Bruno Damasceno Reis
 // 11721EAU008
 
-#include <stdio.h>
-
-int main()
-{	char str[256];
-	char *p;
-	int i, cont, txt[256], opcao;
-	int *ps;
-	do{
-	do{printf("MENU: \n1 - Criptografar \n2 - Descriptografar \n3 - Sair do programa \nSua opcao eh: ");
-	scanf("%d", &opcao);
-	getchar();}while(opcao!=1 && opcao!=2 && opcao!=3);
-	i = 0;
-	switch(opcao)
-	{	case 1:
-			for(i=0; i<256; i++){ str[i] = '\0'; }
-			printf("Digite uma informacao textual: ");
-			fgets(str, 256, stdin);
-			for(cont=0; str[cont] != '\0'; cont++);	
-			ps = (int*)str;
-			printf("\nFrase criptografada: \n");
-			for(i=0; i<cont; i++)	
-			{	if(*(ps+i) != 0 )
-				{	printf("%i ", *(ps+i));	}	} 
-			break;
-			
-		case 2:
-			printf("Quando quiser encerrar a sequencia de numeros, entre com 0\n");
-			p = (char*)txt;
-			i=0; cont=0;
-			printf("Digite uma sequencia de numeros: \n");
-			do
-			{	scanf("%d", &cont);
-	 			getchar();
-				txt[i] = cont;
-				i++;
-			}while(cont != 0);
-			p = (char*)&txt;
-			printf("\nFrase descriptografada: \n");
-			for(i=0; i<sizeof(txt) && *(p+i) != 0; i++)
-			{	printf("%c", *(p+i));		}
-			break;
-
-		default:
-			break;
-	}
-	printf("\n\n");
-	}while(opcao != 3);
-	return 0;
-}
++#include <stdio.h>
++#include <stdlib.h>
++#include <string.h>
++typedef
++	struct Dimensoes
++	{
++		float largura;
++		float profundidade;
++		float altura;
++	}
++Dimensoes_m;
++typedef
++	struct Produto
++	{
++		char nome[64];
++		float preco;
++		Dimensoes_m dimensoes;
++	}
++Produto_m;
++void cadastro(int x, Produto_m p[2])
++{
++	printf("\nNome: ");
++	fgets(p[x].nome, 64, stdin); 
++	p[x].nome[strlen(p[x].nome)-1] = '\0';
++	printf("\nPreco: ");
++	scanf("%f", &p[x].preco);
++	getchar();
++	printf("\nLargura: ");
++	scanf("%f", &p[x].dimensoes.largura);
++	getchar();
++	printf("\nProfundidade: ");
++	scanf("%f", &p[x].dimensoes.profundidade);
++	getchar();
++	printf("\nAltura: ");
++	scanf("%f", &p[x].dimensoes.altura);
++	getchar();
++}
++int main()
++{
++	int op_1=0, op_2=0, i=0;
++	char aux[64];
++	Produto_m p[2];
++	for (i=0; i<2; i++)
++		p[i].nome[0] = '\0';
++	do
++	{
++		printf("\n1 - Cadastro\n2 - Consulta\n....");
++		scanf("%d", &op_1);
++		getchar();
++		switch (op_1)
++		{
++			case 1:
++				printf("\n....1 - Produto 1\n....2 - Produto 2\n........");
++				scanf("%d", &op_2);
++				getchar();
++				printf("\nCadastre as informacoes do produto %d: ", op_2);
++				cadastro(op_2-1, p);
++				printf("\nProduto %d cadastrado com sucesso", op_2);
++				break;
++			case 2:
++				printf("\nProduto a ser procurado: ");
++				scanf("%d", &op_2);
++				getchar();
++				if (strlen(p[op_2-1].nome) == 0)
++					printf("\nProduto nao cadastrado!\n");
++				else
++					printf("\n%s, R$ %.2f, L: %.2fm x P: %.2fm x A: %.2fm", p[op_2-1].nome, p[op_2-1].preco, p[op_2-1].dimensoes.largura, p[op_2-1].dimensoes.profundidade, p[op_2-1].dimensoes.altura);
++				break;
++		}
++	}while (op_1 == 1 || op_1 == 2);
++	return 0;
++}
